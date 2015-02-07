8ad4b276ab2bbf3785640463946f687b801a553b
MiguelJimenez <maj009@gmail.com>
2015-02-07 13:49:56 +0100

    201502071350
    Vídeo 27: hasta el minuto 20 (incluye sql)

---
+++ 06AplicacionCRUD/conexion.php
@@ -0,0 +1,9 @@
+<?php 
+include('config.php');
+
+function conexionMySQL()
+{
+	echo "Hola, por favor no usen echo's para imprimir en pantalla";
+}
+conexionMySQL();
+?>
---
+++ 06AplicacionCRUD/super_heroes.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-02-2015 a las 13:47:55
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `super_heroes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `editorial`
+--
+
+CREATE TABLE IF NOT EXISTS `editorial` (
+  `id_editorial` int(11) NOT NULL AUTO_INCREMENT,
+  `editorial` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_editorial`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=8 ;
+
+--
+-- Volcar la base de datos para la tabla `editorial`
+--
+
+INSERT INTO `editorial` (`id_editorial`, `editorial`) VALUES
+(1, 'DC Comics'),
+(2, 'Marvel Comic'),
+(3, 'Shonen Jump'),
+(4, 'Vértigo'),
+(5, 'Mirage Studio'),
+(6, 'Icon Comics');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `heroes`
+--
+
+CREATE TABLE IF NOT EXISTS `heroes` (
+  `id_heroe` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `imagen` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci,
+  `editorial` int(11) NOT NULL,
+  PRIMARY KEY (`id_heroe`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `heroes`
+--
+
